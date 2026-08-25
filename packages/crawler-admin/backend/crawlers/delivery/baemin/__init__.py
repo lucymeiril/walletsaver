e9657f09d344ed0ec6d101aa331716014dbd19af
오경민 <143767795+lucymeiril@users.noreply.github.com>
2026-08-25 17:47:49 +0900

    chore(crawler): remove abandoned delivery crawlers

--- packages/crawler-admin/backend/crawlers/delivery/baemin/__init__.py
+++
@@ -1,4 +0,0 @@
-"""배달의민족 크롤러 패키지."""
-from .crawler import BaeminCrawler
-
-__all__ = ["BaeminCrawler"]
--- packages/crawler-admin/backend/crawlers/delivery/yogiyo/__init__.py
+++
@@ -1,4 +0,0 @@
-"""요기요 크롤러 패키지."""
-from .crawler import YogiyoCrawler
-
-__all__ = ["YogiyoCrawler"]
